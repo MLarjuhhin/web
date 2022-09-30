b3ad6c825f1f28ab1fbf9dc9a261a83c9e0570c9
Maksim <infojuht@tondiraba.edu.ee>
2022-09-30 19:32:03 +0300

    30.09

--- Tondiraba v1/htdocs/index.php
+++ Tondiraba v1/htdocs/index.php
@@ -1,18 +1,34 @@
 <?php
 
-$x=4;
-$y=159;
+var_dump($_POST);
 
+if(!empty($_POST)){
 
-echo "<input type='text'>";
-echo "<br>";
-echo $x."+".$y."= ".($x+$y);   
-    echo "<br>";
-echo $x."-".$y."= ".($x-$y);
-    echo "<br>";
-echo $x."*".$y."= ".$x*$y;
-    echo "<br>";
-echo $x."/".$y."= ".$x/$y;
+    echo "<br><hr><br>";
+    $x=$_POST['a'];
+    $y=$_POST["b"];
 
- 
+    var_dump($x,$y);
+
+    echo "<br>";
+    echo $x."+".$y."= ".($x+$y);   
+        echo "<br>";
+    echo $x."-".$y."= ".($x-$y);
+        echo "<br>";
+    echo $x."*".$y."= ".$x*$y;
+        echo "<br>";
+    echo $x."/".$y."= ".$x/$y;
+}else{
+    echo "Пост пустой";
+}
 ?>
+<br>
+<html>
+    <body>
+<form method="POST" action="">
+        <input type="text" name="a" value='1'>
+        <input type="text" name="b" value='3'>
+        <input type="submit">
+</form>
+    </body>
+</html>
