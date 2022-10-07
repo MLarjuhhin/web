e4d150e4034c54727ca58162e022c436814bb0f8
Maksim <infojuht@tondiraba.edu.ee>
2022-10-07 17:46:30 +0300

    05.10

--- Tondiraba v1/htdocs/index.php
+++ Tondiraba v1/htdocs/index.php
@@ -1,33 +1,45 @@
 <?php
-
 var_dump($_POST);
-
-if(!empty($_POST)){
-
+if(!empty($_POST) ){
+    
     echo "<br><hr><br>";
     $x=$_POST['a'];
     $y=$_POST["b"];
-
-    var_dump($x,$y);
-
-    echo "<br>";
-    echo $x."+".$y."= ".($x+$y);   
-        echo "<br>";
-    echo $x."-".$y."= ".($x-$y);
-        echo "<br>";
-    echo $x."*".$y."= ".$x*$y;
-        echo "<br>";
-    echo $x."/".$y."= ".$x/$y;
+        if(!empty($x) && !empty($y)){
+            if(is_numeric($x) && is_numeric($y)){
+                echo "<br>";
+                echo $x."+".$y."= ".($x+$y);   
+                    echo "<br>";
+                echo $x."-".$y."= ".($x-$y);
+                    echo "<br>";
+                echo $x."*".$y."= ".$x*$y;
+                    echo "<br>";
+                echo $x."/".$y."= ".$x/$y;
+            }elseif(!is_numeric($x) &&  !is_numeric($y)){
+                echo "Полe Y и Х не должно содержать буквы";  
+            }elseif(!is_numeric($x)){
+                echo "Полe X не должно содержать буквы";   
+            }elseif(!is_numeric($y)){
+                echo "Полe Y не должно содержать буквы";   
+            }elseif(!is_numeric($x) &&  !is_numeric($y)){
+                echo "Полe Y и Х не должно содержать буквы";  
+            }
+            else{
+                echo "Поля не должны содержать буквы";   
+            }
+        }else{
+            echo "Поля не должны быт пустыми";
+        }
 }else{
-    echo "Пост пустой";
+    echo "Пост пуст";
 }
 ?>
 <br>
 <html>
     <body>
 <form method="POST" action="">
-        <input type="text" name="a" value='1'>
-        <input type="text" name="b" value='3'>
+        <input type="text" name="a" value="<?=$x?>">
+        <input type="text" name="b" value="<?=$y?>">
         <input type="submit">
 </form>
     </body>
