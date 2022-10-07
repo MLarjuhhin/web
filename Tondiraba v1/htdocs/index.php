1be830756058b738eedfcb54f8d86aa6c120817e
maksimlarjuhhin <larjuhhin@gmail.com>
2022-10-07 18:17:18 +0300

    ю

--- Tondiraba v1/htdocs/index.php
+++ Tondiraba v1/htdocs/index.php
@@ -1,53 +1,18 @@
 <?php
-var_dump($_POST);
-if(!empty($_POST) ){
 
-    echo "<br><hr><br>";
-    $x=$_POST['a'];
-    $y=$_POST["b"];
-        if(!empty($x) && !empty($y)){
-            if(is_numeric($x) && is_numeric($y)){
-                echo "<br>";
-                echo $x."+".$y."= ".($x+$y);   
-                    echo "<br>";
-                echo $x."-".$y."= ".($x-$y);
-                    echo "<br>";
-                echo $x."*".$y."= ".$x*$y;
-                    echo "<br>";
-                echo $x."/".$y."= ".$x/$y;
-            }elseif(!is_numeric($x) &&  !is_numeric($y)){
-                echo "Полe Y и Х не должно содержать буквы";  
-            }elseif(!is_numeric($x)){
-                echo "Полe X не должно содержать буквы";   
-            }elseif(!is_numeric($y)){
-                echo "Полe Y не должно содержать буквы";   
-            }elseif(!is_numeric($x) &&  !is_numeric($y)){
-                echo "Полe Y и Х не должно содержать буквы";  
-            }
-            else{
-                echo "Поля не должны содержать буквы";   
-            }
-        }else{
-            echo "Поля не должны быт пустыми";
-        }
-}else{
-    echo "Пост пуст";
-}
+$x=4;
+$y=159;
+
+
+echo "<input type='text'>";
+echo "<br>";
+echo $x."+".$y."= ".($x+$y);   
+    echo "<br>";
+echo $x."-".$y."= ".($x-$y);
+    echo "<br>";
+echo $x."*".$y."= ".$x*$y;
+    echo "<br>";
+echo $x."/".$y."= ".$x/$y;
+
+ 
 ?>
-<br>
-<html>
-    <body>
-<form method="POST" action="">
-        <input type="text" name="a" value="<?=$x?>">
-        <select name='select'>
-            <option value="non"></option>  
-            <option value="plus">+</option>
-            <option value="minus">-</option>
-            <option value="division">/</option>
-            <option value="multiplication">*</option>    
-        </select>
-        <input type="text" name="b" value="<?=$y?>">
-        <input type="submit">
-</form>
-    </body>
-</html>
