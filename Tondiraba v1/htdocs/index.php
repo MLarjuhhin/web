51843aec00b480096bb9c25c48faa3d77e6d6f59
maksimlarjuhhin <larjuhhin@gmail.com>
2022-10-07 18:17:44 +0300

    Revert "."
    This reverts commit 148967c1

--- Tondiraba v1/htdocs/index.php
+++ Tondiraba v1/htdocs/index.php
@@ -1,18 +1,46 @@
 <?php
-
-$x=4;
-$y=159;
-
-
-echo "<input type='text'>";
-echo "<br>";
-echo $x."+".$y."= ".($x+$y);   
-    echo "<br>";
-echo $x."-".$y."= ".($x-$y);
-    echo "<br>";
-echo $x."*".$y."= ".$x*$y;
-    echo "<br>";
-echo $x."/".$y."= ".$x/$y;
-
- 
+var_dump($_POST);
+if(!empty($_POST) ){
+    
+    echo "<br><hr><br>";
+    $x=$_POST['a'];
+    $y=$_POST["b"];
+        if(!empty($x) && !empty($y)){
+            if(is_numeric($x) && is_numeric($y)){
+                echo "<br>";
+                echo $x."+".$y."= ".($x+$y);   
+                    echo "<br>";
+                echo $x."-".$y."= ".($x-$y);
+                    echo "<br>";
+                echo $x."*".$y."= ".$x*$y;
+                    echo "<br>";
+                echo $x."/".$y."= ".$x/$y;
+            }elseif(!is_numeric($x) &&  !is_numeric($y)){
+                echo "Полe Y и Х не должно содержать буквы";  
+            }elseif(!is_numeric($x)){
+                echo "Полe X не должно содержать буквы";   
+            }elseif(!is_numeric($y)){
+                echo "Полe Y не должно содержать буквы";   
+            }elseif(!is_numeric($x) &&  !is_numeric($y)){
+                echo "Полe Y и Х не должно содержать буквы";  
+            }
+            else{
+                echo "Поля не должны содержать буквы";   
+            }
+        }else{
+            echo "Поля не должны быт пустыми";
+        }
+}else{
+    echo "Пост пуст";
+}
 ?>
+<br>
+<html>
+    <body>
+<form method="POST" action="">
+        <input type="text" name="a" value="<?=$x?>">
+        <input type="text" name="b" value="<?=$y?>">
+        <input type="submit">
+</form>
+    </body>
+</html>
