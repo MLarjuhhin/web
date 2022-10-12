e4b6ce156eb9087263a3297de37013f872052583
Maksim <infojuht@tondiraba.edu.ee>
2022-10-12 18:02:27 +0300

    .

--- Tondiraba v1/htdocs/index.php
+++ Tondiraba v1/htdocs/index.php
@@ -5,16 +5,39 @@ if(!empty($_POST) ){
     echo "<br><hr><br>";
     $x=$_POST['a'];
     $y=$_POST["b"];
-        if(!empty($x) && !empty($y)){
+    $symbol=$_POST['select'];
+
+
+        if(!empty($x) && !empty($y)  && $symbol!='non'){
             if(is_numeric($x) && is_numeric($y)){
                 echo "<br>";
-                echo $x."+".$y."= ".($x+$y);   
-                    echo "<br>";
-                echo $x."-".$y."= ".($x-$y);
-                    echo "<br>";
-                echo $x."*".$y."= ".$x*$y;
-                    echo "<br>";
-                echo $x."/".$y."= ".$x/$y;
+                // echo $x."+".$y."= ".($x+$y);   
+                //     echo "<br>";
+                // echo $x."-".$y."= ".($x-$y);
+                //     echo "<br>";
+                // echo $x."*".$y."= ".$x*$y;
+                //     echo "<br>";
+                // echo $x."/".$y."= ".$x/$y;
+
+                    switch($symbol){
+                        case 'minus':
+                            echo $x."-".$y."= ".($x-$y);
+                            $selected=$symbol;
+                            break;
+                        case 'plus':
+                            echo $x."+".$y."= ".($x+$y); 
+                            $selected=$symbol; 
+                            break;
+                        case 'division':
+                            echo $x."/".$y."= ".$x/$y;
+                            $selected=$symbol;
+                            break;
+                        case 'multiplication':
+                            echo  $x."*".$y."= ".$x*$y;
+                            $selected=$symbol;
+                            break;
+                    }
+
             }elseif(!is_numeric($x) &&  !is_numeric($y)){
                 echo "Полe Y и Х не должно содержать буквы";  
             }elseif(!is_numeric($x)){
@@ -42,11 +65,11 @@ if(!empty($_POST) ){
 <form method="POST" action="">
         <input type="text" name="a" value="<?=$x?>">
         <select name='select'>
-            <option value=""></option>  
-            <option value="plus">+</option>
-            <option value="minus">-</option>
-            <option value="division">/</option>
-            <option value="multiplication">*</option>    
+            <option value="non" ></option>  
+            <option value="plus" <?=($symbol=='plus')?'selected':''?>>+</option>
+            <option value="minus" <?=($symbol=='minus')?'selected':''?>>-</option>
+            <option value="division" <?=($symbol=='division')?'selected':''?>>/</option>
+            <option value="multiplication" <?=($symbol=='multiplication')?'selected':''?>>*</option>    
         </select>
         <input type="text" name="b" value="<?=$y?>">
         <input type="submit">
