5d073531156be6457ded2e716f4734957e249810
maksimlarjuhhin <larjuhhin@gmail.com>
2022-10-14 17:51:34 +0300

    12-10

--- Tondiraba v1/htdocs/index.php
+++ Tondiraba v1/htdocs/index.php
@@ -5,16 +5,26 @@ if(!empty($_POST) ){
     echo "<br><hr><br>";
     $x=$_POST['a'];
     $y=$_POST["b"];
-        if(!empty($x) && !empty($y)){
+    $symbol=$_POST['select'];
+
+        if(!empty($x) && !empty($y)  && $symbol!='non'){
             if(is_numeric($x) && is_numeric($y)){
                 echo "<br>";
-                echo $x."+".$y."= ".($x+$y);   
-                    echo "<br>";
-                echo $x."-".$y."= ".($x-$y);
-                    echo "<br>";
-                echo $x."*".$y."= ".$x*$y;
-                    echo "<br>";
-                echo $x."/".$y."= ".$x/$y;
+                    switch($symbol){
+                        case 'minus':
+                            echo $x."-".$y."= ".($x-$y);
+                            break;
+                        case 'plus':
+                            echo $x."+".$y."= ".($x+$y);
+                            break;
+                        case 'division':
+                            echo $x."/".$y."= ".$x/$y;
+                            break;
+                        case 'multiplication':
+                            echo  $x."*".$y."= ".$x*$y;
+                            break;
+                    }
+
             }elseif(!is_numeric($x) &&  !is_numeric($y)){
                 echo "Полe Y и Х не должно содержать буквы";  
             }elseif(!is_numeric($x)){
@@ -40,15 +50,15 @@ if(!empty($_POST) ){
 
     <body>
 <form method="POST" action="">
-        <input type="text" name="a" value="<?=$x?>">
-        <select name='select'>
-            <option value=""></option>  
-            <option value="plus">+</option>
-            <option value="minus">-</option>
-            <option value="division">/</option>
-            <option value="multiplication">*</option>    
+        <input type="text" name="a" value="<?=(isset($x))?$x:''?>">
+        <select >
+            <option value="non" ></option>  
+            <option value="plus" <?=($symbol=='plus')?'selected':''?>>+</option>
+            <option value="minus" <?=($symbol=='minus')?'selected':''?>>-</option>
+            <option value="division" <?=($symbol=='division')?'selected':''?>>/</option>
+            <option value="multiplication" <?=($symbol=='multiplication')?'selected':''?>>*</option>    
         </select>
-        <input type="text" name="b" value="<?=$y?>">
+        <input type="text" name="b" value="<?=(isset($y))?$y:''?>">
         <input type="submit">
 </form>
     </body>
