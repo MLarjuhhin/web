487962f4b4b850c990b6cb105126378a645a7c8f
maksimlarjuhhin <larjuhhin@gmail.com>
2023-03-24 18:57:06 +0200

    22.03

--- Tondiraba v2/htdocs/admin/scripts/product.php
+++ Tondiraba v2/htdocs/admin/scripts/product.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($modulePage2 == 'add' || ($modulePage2 == 'edit' && is_numeric($modulePage3))) {
+if ($modulePage2 == 'add' || ($modulePage2 == 'edit' && is_numeric($modulePage3)) || ($modulePage2 == 'delete' && is_numeric($modulePage3))) {
 	$url = false;
 	//SELECT
 	$Update = Product::getProductByID($DB, $modulePage3);
@@ -17,30 +17,17 @@ if ($modulePage2 == 'add' || ($modulePage2 == 'edit' && is_numeric($modulePage3)
 		} elseif ($_POST['act'] == 'edit_product') {
 			$product_data['id'] = $modulePage3;
 			$product_data['date_edit'] = $DB->time();
+		}elseif($_POST['act'] == 'delete_product'){
+			$product_data['date_delete'] = $DB->time();
+			$product_data['id'] = $modulePage3;
+			$url = '/'.$modulePage0."/".$modulePage1;
 		}
 		if (Product::save($DB, $product_data)) {
 			$_SESSION['success'] = 'ok';
 		} else {
-			//mail('infojuht@tondiraba.edu.com','product_add_error',print_r([$_POST,$_SESSION,$product_data],true));
 			$_SESSION['error'] = 'error';
 		}
 		refreshPage($url);
 	}
-} elseif ($modulePage2 == 'delete' && is_numeric($modulePage3)) {
-	$product_data['date_delete'] = $DB->time();
-	$product_data['id'] = $modulePage3;
-
-	$url = '/'.$modulePage0."/".$modulePage1;
-
-	if (Product::save($DB, $product_data)) {
-		$_SESSION['success'] = 'ok';
-	} else {
-		//mail('infojuht@tondiraba.edu.com','product_add_error',print_r([$_POST,$_SESSION,$product_data],true));
-		$_SESSION['error'] = 'error';
-	}
-
-	refreshPage($url);
-
-
 }
 
