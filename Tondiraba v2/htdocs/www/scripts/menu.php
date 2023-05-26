437e3e5c83b774a6b5d13e0cc82b06665f3f6567
maksimlarjuhhin <larjuhhin@gmail.com>
2023-05-26 18:47:51 +0300

    .

--- Tondiraba v2/htdocs/www/scripts/menu.php
+++ Tondiraba v2/htdocs/www/scripts/menu.php
@@ -1,3 +1,10 @@
 <?php
 $category=$DB->AllRows("SELECT *  FROM category WHERE date_delete is null");
 
+foreach ($category as $k=>$c){
+	// $c['id'] = id category
+		$dishQuery=$DB->AllRows("SELECT id,name,price FROM dish where category_id=? and date_delete is null ",[$c['id']]);
+		if(!empty($dishQuery)){
+			$dish[$c['id']]=$dishQuery;
+		}
+}
--- Tondiraba v2/htdocs/www/tpl/menu_tpl.php
+++ Tondiraba v2/htdocs/www/tpl/menu_tpl.php
@@ -17,172 +17,53 @@
             <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Menu & Pricing</h4>
             <h1 class="display-4">Competitive Pricing</h1>
         </div>
-        <div class="row">
-            <div class="col-lg-6">
-                <h1 class="mb-5">Горячие напитки</h1>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-1.jpg" alt="">
-                        <h5 class="menu-price">$5</h5>
+
+        <?
+		$x = 1;
+		foreach ($dish as $k => $v){
+		if ($x == 1){
+		?>
+        <div class='row'><? } ?>
+            <div class='col-lg-6'>
+                <h1 class="mb-5"><?= $k ?></h1>
+				<? foreach ($v as $k1 => $v2) {
+					?>
+                    <div class="row align-items-center mb-5">
+                        <div class="col-4 col-sm-3">
+                            <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
+                            <h5 class="menu-price"><?= $v2['price'] ?></h5>
+                        </div>
+                        <div class="col-8 col-sm-9">
+                            <h4><?= $v2['name'] ?></h4>
+                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem
+                                dolor</p>
+                        </div>
                     </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Black Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-2.jpg" alt="">
-                        <h5 class="menu-price">$7</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Chocolete Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
+				<? } ?>
             </div>
-            <div class="col-lg-6">
-                <h1 class="mb-5">Холодные напитки</h1>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-1.jpg" alt="">
-                        <h5 class="menu-price">$5</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Black Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-2.jpg" alt="">
-                        <h5 class="menu-price">$7</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Chocolete Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6">
-                <h1 class="mb-5">Еда</h1>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-1.jpg" alt="">
-                        <h5 class="menu-price">$5</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Black Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-2.jpg" alt="">
-                        <h5 class="menu-price">$7</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Chocolete Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <h1 class="mb-5">Соки</h1>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-1.jpg" alt="">
-                        <h5 class="menu-price">$5</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Black Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-2.jpg" alt="">
-                        <h5 class="menu-price">$7</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Chocolete Coffee</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-                <div class="row align-items-center mb-5">
-                    <div class="col-4 col-sm-3">
-                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">
-                        <h5 class="menu-price">$9</h5>
-                    </div>
-                    <div class="col-8 col-sm-9">
-                        <h4>Coffee With Milk</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+			<? if ($x == 2) {
+				echo "</div >";
+				$x = 0;
+			}
+			$x++;
+			} ?>
+
     </div>
 </div>
+
 <!-- Menu End -->
+
+
+<!--    --><?// foreach ($v as $k1=>$v2){?>
+<!--                <div class="row align-items-center mb-5">-->
+<!--                    <div class="col-4 col-sm-3">-->
+<!--                        <img class="w-100 rounded-circle mb-3 mb-sm-0" src="img/menu-3.jpg" alt="">-->
+<!--                        <h5 class="menu-price">--><?//=$v2['price']?><!--</h5>-->
+<!--                    </div>-->
+<!--                    <div class="col-8 col-sm-9">-->
+<!--                        <h4>--><?//=$v2['name']?><!--</h4>-->
+<!--                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor</p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!--            --><?//}?>
