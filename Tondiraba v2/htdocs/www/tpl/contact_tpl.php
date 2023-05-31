a9f1afd3ce8fba85bd7a8566631c80c80d099630
maksimlarjuhhin <larjuhhin@gmail.com>
2023-05-31 18:21:33 +0300

    .

---
+++ Tondiraba v2/htdocs/www/tpl/contact_tpl.php
@@ -0,0 +1,79 @@
+
+
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 position-relative overlay-bottom">
+	<div class="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style="min-height: 400px">
+		<h1 class="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">Contact</h1>
+		<!-- <div class="d-inline-flex mb-lg-5">
+			<p class="m-0 text-white"><a class="text-white" href="">Home</a></p>
+			<p class="m-0 text-white px-2">/</p>
+			<p class="m-0 text-white">Contact</p>
+		</div> -->
+	</div>
+</div>
+<!-- Page Header End -->
+
+
+<!-- Contact Start -->
+<div class="container-fluid pt-5">
+	<div class="container">
+		<div class="section-title">
+			<h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Contact Us</h4>
+			<h1 class="display-4">Feel Free To Contact</h1>
+		</div>
+		<div class="row px-3 pb-2">
+			<div class="col-sm-4 text-center mb-3">
+				<i class="fa fa-2x fa-map-marker-alt mb-3 text-primary"></i>
+				<h4 class="font-weight-bold">Address</h4>
+				<p>123 Street, New York, USA</p>
+			</div>
+			<div class="col-sm-4 text-center mb-3">
+				<i class="fa fa-2x fa-phone-alt mb-3 text-primary"></i>
+				<h4 class="font-weight-bold">Phone</h4>
+				<p>+012 345 6789</p>
+			</div>
+			<div class="col-sm-4 text-center mb-3">
+				<i class="far fa-2x fa-envelope mb-3 text-primary"></i>
+				<h4 class="font-weight-bold">Email</h4>
+				<p>info@example.com</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6 pb-5">
+				<iframe style="width: 100%; height: 443px;"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2028.6887433308534!2d24.850340777367066!3d59.43826560229585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4692ec955169225f%3A0xbdf3c4c453be5fc0!2sTondiraba%20Huvikool!5e0!3m2!1set!2see!4v1685546361196!5m2!1set!2see" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			<div class="col-md-6 pb-5">
+				<div class="contact-form" >
+					<div id="success"></div>
+					<form name="sentMessage" id="contactForm" >
+						<div class="control-group">
+							<input type="text" class="form-control bg-transparent p-4" id="name" placeholder="Your Name"
+								   required="required" >
+							<p class="help-block text-danger"></p>
+						</div>
+						<div class="control-group">
+							<input type="email" class="form-control bg-transparent p-4" id="email" placeholder="Your Email"
+								   required="required" />
+							<p class="help-block text-danger"></p>
+						</div>
+						<div class="control-group">
+							<input type="text" class="form-control bg-transparent p-4" id="subject" placeholder="Subject"
+								   required="required" />
+							<p class="help-block text-danger"></p>
+						</div>
+						<div class="control-group">
+                                <textarea class="form-control bg-transparent py-3 px-4" rows="5" id="message" placeholder="Message"
+										  required="required"></textarea>
+							<p class="help-block text-danger"></p>
+						</div>
+						<div>
+							<button class="btn btn-primary font-weight-bold py-3 px-5" type="submit" id="sendMessageButton">Send
+								Message</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Contact End -->
